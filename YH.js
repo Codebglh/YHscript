13b046bf7c263aa9dab40123b4530dda20fb4156
bgcode <bglhcode@qq.com>
2023-07-21 22:28:19 +0800

    bg

--- YH.js
+++ YH.js
@@ -1,27 +1,27 @@
-// function en() {
-//    // $(".bgcode").hide()
-//
-//     var lists=new Array();
-//
-//
-//         var x=0;
-//         var list = $("body").text().match(/htt.*?m3u8/g);
-//         for (var i=0;i<list.length;i++){
-//             if(list[i]!="htt.*?m3u8"){
-//                 lists[x]=list[i];
-//                 x=x+1;
-//             }
-//         }
-//
-//
-//     $("body").append(`< div className = "box" >< div className = "bg-movie" id = "dplayer" > < /div> <div className="list-bg"></div></div>`)
-//
-//
-//     for (var i = 0; i < lists.length; i++) {
-//             $(".list-bg").append("<input onclick=\"opensss('"+ lists[i] + "')\" type=\"button\" value=第" + (i + 1) + "集>")
-//             console.log(list[i])
-//     }
-// }
+function en() {
+   // $(".bgcode").hide()
+
+    var lists=new Array();
+
+
+        var x=0;
+        var list = $("body").text().match(/htt.*?m3u8/g);
+        for (var i=0;i<list.length;i++){
+            if(list[i]!="htt.*?m3u8"){
+                lists[x]=list[i];
+                x=x+1;
+            }
+        }
+
+
+    $("body").append(`< div className = "box" >< div className = "bg-movie" id = "dplayer" > < /div> <div className="list-bg"></div></div>`)
+
+
+    for (var i = 0; i < lists.length; i++) {
+            $(".list-bg").append("<input onclick=\"opensss('"+ lists[i] + "')\" type=\"button\" value=第" + (i + 1) + "集>")
+            console.log(list[i])
+    }
+}
 function sasa(){
     console.log("sasa")
 }
