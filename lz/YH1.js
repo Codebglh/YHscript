938477ce12a2f9dcecaf5e1a7b67bf144f0180d5
bgcode <bglhcode@qq.com>
2023-07-21 23:48:18 +0800

    bg4

--- lz/YH1.js
+++ lz/YH1.js
@@ -7,7 +7,7 @@ function en() {
             if(list[i]!="htt.*?m3u8"){
                 lists[x]=list[i];
                 x=x+1;
-                $(".box").append("<div className=\"bg-movie\" id =\"dplayer\" > </div><div class=\"list-bg\"></div>")
+                $(".box").append("<div class=\"bg-movie\" id =\"dplayer\" > </div><div class=\"list-bg\"></div>")
                 $(".list-bg").append("<input onclick=\"opensss('"+ lists[i] + "')\" type=\"button\" value=\"第" + (i + 1) + "集>")
                 console.log(lists[i])
             }
