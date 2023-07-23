1f3a27ba3b39feda54bef2072a5a3ee4de0edc63
bgcode <bglhcode@qq.com>
2023-07-23 18:24:23 +0800

    bg4

---
+++ lz/YH.js
@@ -0,0 +1,48 @@
+var box=true;
+function en() {
+
+    if (box){
+        box=false;
+        if ($(".box").length==0) {
+            $("body").append("<div class='box'>  <div class=\"bg-movie\" id =\"dplayer\" > </div><div class=\"list-bg\"></div></div>")
+            var lists=new Array();
+            var x=0;
+            var list = $("body").text().match(/htt.*?m3u8/g);
+            for (var i=0;i<list.length;i++){
+                if(list[i]!="htt.*?m3u8"){
+                    lists[x]=list[i];
+                    x=x+1;
+                }
+                $(".list-bg").append('<input onclick="openmovie(\''+ lists[i] + '\')" type="button" value="第 '+ (i + 1) +' 集">')
+            }
+        }
+    }else {
+        $(".box").remove();
+        box=true;
+    }
+
+
+
+
+
+}
+
+function openmovie(url) {
+    const dp = new DPlayer({
+        container: document.getElementById("dplayer"),
+        video: {
+            url: url,
+            type: "customHls",
+            customType: {
+                customHls: function (video, player) {
+                    const hls = new Hls();
+                    hls.loadSource(video.src);
+                    hls.attachMedia(video);
+                },
+            },
+        },
+    });
+}
+
+
+
